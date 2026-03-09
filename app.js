b06f960da2836aba5338f871789ac802863acf39
SabrinaAllen <sabriinaallen@gmail.com>
2026-03-09 01:10:01 +0000

    loops

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const tags = ["js", "week3"]
+const scores = [10, 25, 18, 30]
+
+let totalScore = 0;
+
+for (let i = 0; i < scores.length; i++) {
+    totalScore += scores[i];
+}
+console.log(totalScore)
